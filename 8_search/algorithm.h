b90afcd7a5a1d1d561b793fc6983ad07235ae74b
zhangyiheng04 <zhangyiheng04@gmail.com>
2017-04-25 15:54:48 +0800

    ..

--- 8_search/algorithm.h
+++ 8_search/algorithm.h
@@ -44,5 +44,77 @@ int Binary_Search(int *a, int n, int key)
 
 int Fibonacci_Search(int *a, int n, int key)
 { /*斐波那契查找*/
+	int low, high, mid, i, k;
+	low = 1;
+	high = n;
+	k = 0;
+	while (n >F[k]-1) //计算n位于数列的位置
+		++k;
+	for (i=n; i < F[k]-1; ++i) //将不满的数值补全
+		a[i] = a[n];
 
+	while (low <= high)
+	{
+		mid = low+F[k-1]-1; //计算当前分隔的下标
+		if (key < a[mid]) //
+		{
+			high = mid-1;
+			k = k-1;
+		}
+		else if (key > a[mid])
+		{
+			low = mid+1;
+			k -= 2;
+		}
+		else
+		{
+			if (mid <= n)
+				return mid;
+			else 
+				return n;
+		}
+	}
+
+	return 0;
 }
+
+/*二叉排序树*/
+typedef struct BiTNode
+/*二叉树的二叉链表结点结构定义*/
+{
+	int data;
+	struct BiTNode *lchild, *rchild;
+}BiTNode, *BiTree;
+
+Status SearchBST(BiTree T, int key, BiTree f, BiTree *p)
+{
+	/*
+	 可递归函数，关键点在于f 是 T 的双亲结点
+	 方便查找兄弟结点
+	 * */
+	if (!T)
+	{
+		*p = f;
+		return FALSE;
+	}
+	else if (key == T->data)
+	{
+		*p = T;
+		return TRUE;
+	}
+	else if (key < T->data)
+		return SearchBST(T->lchild, key, T, p);
+	else
+		return SearchBST(T->rchild, key, T, p);
+}
+
+
+
+
+
+
+
+
+
+
+
